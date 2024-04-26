085f719af72b9b0230755963cdd28eccec150932
ruskyvisky <numan_859@hotmail.com>
2024-04-26 20:34:41 +0300

    Add demo.java file to the controller package

---
+++ src/main/java/com/travis/demo/controller/demo.java
@@ -0,0 +1,11 @@
+package com.travis.demo.controller;
+
+/**
+ * demo
+ */
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Travis CI");
+    }
+}
